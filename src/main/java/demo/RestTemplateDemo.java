5f3a36db4319c679f585f8b65953428354c8a2b8
mattesja <mattesjam@googlemail.com>
2024-11-19 21:46:00 +0100

    springboot3

--- src/main/java/demo/RestTemplateDemo.java
+++ src/main/java/demo/RestTemplateDemo.java
@@ -1,10 +1,9 @@
 package demo;
 
-import org.apache.http.HttpHost;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.message.BasicHeader;
+import org.apache.hc.core5.http.HttpHost;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.message.BasicHeader;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
@@ -16,38 +15,26 @@ public class RestTemplateDemo {
 
   public static void main(String[] args) {
     HttpHost proxy = new HttpHost("localhost", 8888);
-    RequestConfig config = RequestConfig.custom()
+    CloseableHttpClient client = HttpClients.custom()
         .setProxy(proxy)
-        .build();
-    HttpClient client = HttpClientBuilder.create()
-        .setDefaultRequestConfig(config)
         //.setDefaultHeaders(Collections.singletonList(new BasicHeader("Connection", "close")))
         .build();
     HttpComponentsClientHttpRequestFactory httpComponentsClientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory(client);
 
     RestTemplate myResttemplate = new RestTemplateBuilder().requestFactory(() -> httpComponentsClientHttpRequestFactory).build();
+    System.out.println("1. call");
+    callPost(myResttemplate);
+    System.out.println("2. call");
+    callPost(myResttemplate);
+  }
+
+  static void callPost(RestTemplate myResttemplate) {
     String url = "https://httpbin.org/post";
     try {
-      myResttemplate.postForObject(
-          url,
-          new HashMap<>(),
-          String.class);
+      myResttemplate.postForObject(url, new HashMap<>(), String.class);
     }
     catch (Exception e) {
       e.printStackTrace();
     }
-    try {
-      //Thread.sleep(2000);
-      myResttemplate.postForObject(
-          url,
-          new HashMap<>(),
-          String.class);
-
-    }
-    catch (Exception e) {
-      e.printStackTrace();
-    }
-
   }
 }
-
